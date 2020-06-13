cea465c3ab2c0e97f8dd4757e22f234c12644e34
David-Vasquez-96 <mariodavid98@hotmail.com>
2020-06-13 12:46:54 -0600

    se creo el proyecto en vue.js y vuetify.js y se integro el Agente Virtual de Dialogflow para poder usarlo en la pagina web, parte final de la tercera entrega del proyecto

---
+++ MariosPizzaConVue.js/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+      flat dense
+    >
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ MariosPizzaConVue.js/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="6" lg="6" md="12" xs="12" sm="12">
+        <v-img
+          src="http://deejoe.etrumeus.com/update/images/pizza-1502140004.svg"
+          class="my-1"
+          contain
+          height="200"
+        />
+        <h1 class="display-2 font-weight-bold mb-3">
+          Bienvenidos a Mario's Pizza
+        </h1>
+        <p class="subheading font-weight-regular">
+          Puedes comunicarte con nosotros y
+          <br>realizar tu orden mediante nuestro asistente virtual
+          <br>o desde nuestra página de <a href="https://www.facebook.com/Marios-Pizza-106498847754240/?view_public_for=106498847754240" target="_blank">Facebook</a>
+        </p>
+      </v-col>
+
+      <v-col cols="6" lg="6" md="12" xs="12" sm="12">
+        <div style="text-align: center;">
+            <iframe class="borderAV" width="350" height="530" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/b96f1195-0163-46aa-8561-211cb3ee9f06"></iframe>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+
+    }),
+  }
+</script>
+
+<style>
+  .borderAV {
+    border-color: black;
+  }
+</style>
